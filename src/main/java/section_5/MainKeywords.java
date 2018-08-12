1fa1a5dd2cb45ab49694674ef5690f96e9ecf8c5
Jarek Kijko/kijek1979 <jwkjk@o2.pl>
2018-08-12 15:35:33 +0200

    first commit

---
+++ src/main/java/section_5/MainKeywords.java
@@ -0,0 +1,11 @@
+package section_5;
+
+public class MainKeywords {
+
+    public static void main(String[] args) {
+        // expressions are built with values, variables, operators and method calls
+        //statements are built with expressions and datatypes
+
+        double y = 45;
+    }
+}
